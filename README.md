81bf6da88802fce76e70cba8fff184e2dbc243fa
seanbreaksintotech <sbyrdie123@gmail.com>
2022-01-12 22:40:31 -0500

    ReadME.md and app.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors-
+A simple project to learn basic problem solving with javascript.
+This game will play against a computer that will randomly return a move based on the users input.  
---
+++ app.js
@@ -0,0 +1,31 @@
+// Function for computer to choose
+// ‘Rock’, ‘Paper’ or ‘Scissors’
+
+
+let computerPlay =["Rock", "Paper", "Scissors"]
+let random = Math.floor(Math.random() * computerPlay.length);
+
+
+// player select
+
+//  const playerSelection = playerPlay()
+// const computerSelection= computerPlay()
+
+//prompt for player to enter only allowed names
+let playerChoose = prompt ("Rock, Paper, Scissors ?");
+if (playerChoose === 'Rock'){
+
+}
+else if (playerChoose === 'Paper'){
+
+}
+else if (playerChoose === 'Scissors'){
+
+}
+
+    else {
+            alert ( 'Invalid selection')
+    }
+    
+     
+
