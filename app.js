b5d957ea9c65a22a63b5de2f271f800a3ba7b852
seanbreaksintotech <sbyrdie123@gmail.com>
2022-01-14 19:35:59 -0500

    app,js

--- app.js
+++ app.js
@@ -6,16 +6,51 @@ const select = ['rock', 'paper', 'scissors']
 function computerPlay (){
 return select[Math.floor(Math.random()*select.length)]
 }
+// function playerPlay (){
+// let playerChoose = prompt ('Rock, Paper or Scissors?')
+    
 
-// defining computer and player selection, allowing playerSelection to be case insensitive 
-const computerSelection = computerPlay;
-const playerSelection= "rock"
-
-playerSelection = playerSelection.toLowerCase
-
+// }
 
 //function that plays a round and returns a string for the winner based on match
-function playRound (computerSelection, playerSelection );{
+function playRound (){
+const playerSelection = "rock"
+const computerSelection = computerPlay ();
 
 
+//  playerSelection = playerSelection.toLowerCase
+    if( computerSelection === playerSelection ){
+    return('its a draw !') }
+    else if (computerSelection === "rock" && playerSelection === "scissors")
+    return('you lose, rock beats scissors!')
+    
+    else if (computerSelection === "scissors" && playerSelection === "rock")
+        return('you win, rock beats scissors!')
+    
+    else if (computerSelection === "paper" && playerSelection === "rock")
+        return('you lose, paper beats rock!')
+    
+    else if (computerSelection === "rock" && playerSelection === "paper")
+        return('you win, paper beats rock!')     
+
+else if (computerSelection === "scissors" && playerSelection === "paper")
+    return('you lose, scissors beats paper!')
+
+else if ( computerSelection === "paper" && playerSelection === "scissors")
+    return('you win, scissors beats paper!') 
+
+  else {
+      return("error")
+  }
 }
+
+
+//5 round game that displays results to console 
+function game(){
+    console.log (playRound())
+    console.log (playRound())
+    console.log (playRound())
+    console.log (playRound())
+    console.log (playRound())
+}
+console.log (game())
